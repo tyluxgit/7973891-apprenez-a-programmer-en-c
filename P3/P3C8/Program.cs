b0692d72fdaf4aa3ac79b11d173006098cd6c611
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-21 23:52:51 +0100

    Amélioration des classes Account et AccountHolder
    Ajout de la gestion des exceptions pour les identifiants et les soldes dans `Account.cs`. Transformation des propriétés `Identifiant`, `Solde` et `Titulaire` en propriétés en lecture seule. Introduction de méthodes pour déposer et retirer des fonds.
    
    Réécriture de la classe `AccountHolder` dans `AccountHolder.cs` pour utiliser des propriétés immuables et validation des paramètres d'entrée. Remplacement de `Age` par `DateNaissance`.
    
    Mise à jour de `Program.cs` pour intégrer la méthode `LireClientParId`, permettant de récupérer les données des clients à partir d'un fichier JSON, avec des messages d'invite pour l'utilisateur.
    
    Ajout du fichier `FichierClientFictifs.json` contenant des données fictives pour les tests.

--- P3/P3C8/Program.cs
+++ P3/P3C8/Program.cs
@@ -1,25 +1,45 @@
-﻿using P3C8.Models;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text.Json;
+using P3C8.Models;
+
 
 class Program
 {
     static void Main()
     {
         // TODO: L'utilisateur doit appuyer sur Entrée pour continuer
+        Console.WriteLine("Appuyez sur Entrée pour continuer...");
+        Console.ReadLine();
+        Console.WriteLine("Saisissez votre identifiant puis Entrée");
+        string identifiant = Console.ReadLine();
 
-        // TODO: L'utilisateur doit saisir un identifiant de compte pour afficher le menu
+        if (int.TryParse(identifiant, out int idRecherche))
+        {
+            AccountHolder clientData = LireClientParId("FichierClientFictifs.json", idRecherche);
 
-        /* TODO: Afficher un menu avec un message et les options suivantes: 
-         Veuillez sélectionner une option ci-dessous :
-        [I] Voir les informations sur le titulaire du compte
-        [CS] Compte courant - Consulter le solde
-        [CD] Compte courant - Déposer des fonds
-        [CR] Compte courant - Retirer des fonds
-        [ES] Compte épargne - Consulter le solde
-        [ED] Compte épargne - Déposer des fonds
-        [ER] Compte épargne - Retirer des fonds
-        [X] Quitter*/
+            if (clientData != null)
+            {
+                /* TODO: Afficher un menu avec un message et les options suivantes: 
+                     Veuillez sélectionner une option ci-dessous :
+                    [I] Voir les informations sur le titulaire du compte
+                    [CS] Compte courant - Consulter le solde
+                    [CD] Compte courant - Déposer des fonds
+                    [CR] Compte courant - Retirer des fonds
+                    [ES] Compte épargne - Consulter le solde
+                    [ED] Compte épargne - Déposer des fonds
+                    [ER] Compte épargne - Retirer des fonds
+                    [X] Quitter*/
 
-        //TODO: L'utilisateur doit saisir une option du menu (majuscule ou minuscule) et appuyer sur Entrée, le programme doit afficher un message d'erreur si l'option n'est pas valide
+                //TODO: L'utilisateur doit saisir une option du menu (majuscule ou minuscule) et appuyer sur Entrée, le programme doit afficher un message d'erreur si l'option n'est pas valide
+            }
 
+        }
+        else
+        {
+            Console.WriteLine("L'identifiant doit être un nombre entier.");
+
+        }
     }
 }
