1b394f76e7451af413663b18c544b421a6c1b88f
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-21 11:39:34 +0100

    Mise à jour des fichiers de configuration et du code
    Ajout de chemins dans `.gitignore` pour ignorer les fichiers de débogage et de configuration de Visual Studio pour les projets P1C2.3, P1C3.1, P1C3.2 et P1C4.
    
    Dans `Program.cs`, ajout de l'espace de noms `P1C4`, instanciation d'un objet `Livre` avec des valeurs pour `Titre`, `Auteur` et `NombreDePages`, et affichage de ces valeurs dans la console. Modification d'un commentaire pour clarifier l'étape d'instanciation.

--- P1/P1C4/Program.cs
+++ P1/P1C4/Program.cs
@@ -1,6 +1,8 @@
-﻿// TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
-
+﻿using P1C4;
+// TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
 // TODO, étape 2 : affecter une valeur aux champs titre, auteur et nombre de pages de votre objet
+var monLivre = new Livre() { Titre = "Le Problème à trois corps", Auteur = "Liu Cixin", NombreDePages = 450 };
+
 
 // Afficher le contenu des champs de cet objet 
 Console.WriteLine("Le titre du livre est " + monLivre.Titre);
