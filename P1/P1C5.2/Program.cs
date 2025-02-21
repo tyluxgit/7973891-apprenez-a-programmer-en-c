f32bc4e01aa57e0514d75ec7570e4ac31c4d433b
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-21 12:01:31 +0100

    Mise à jour de la gestion des invités dans Program.cs
    Création d'une liste de chaînes `invites` et ajout de Joe, Martin et Marie.
    Remplacement de Martin par Jean et suppression de Joe.
    Affichage de la taille de la liste et des invités sur une seule ligne.

--- P1/P1C5.2/Program.cs
+++ P1/P1C5.2/Program.cs
@@ -1,22 +1,24 @@
 ﻿// TODO : Remplacer le ?? par le code approprié pour créer une liste de chaînes
 
-IList <??> invites = new ?? ();
+List <string> invites = new List<string> ();
 
 // TODO : Ajouter Joe, Martin et Marie à la liste.
-
+invites.Add("Joe");
+invites.Add("Martin");
+invites.Add("Marie");
 
 // TODO : Compléter l'instruction suivante en remplaçant le ?? pour afficher la taille de la liste
-Console.WriteLine(invites.??);
+Console.WriteLine("Nombre d'invités : " + invites.Count);
 
 // TODO : Remplacer Martin par Jean dans la liste
-
+invites.Insert(invites.IndexOf("Martin"),"Jean");
 
 // TODO : Retirer Joe de la liste
-
+invites.Remove("Joe");
 
 // Afficher le contenu de la liste
-Console.WriteLine("Les invités sont :");
+Console.Write("Les invités sont :");
 foreach (string invite in invites)
 {
-    Console.WriteLine(invite);
+    Console.Write(" " + invite);
 }
