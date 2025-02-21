1b94d0dbbcc20498c8b0d58197b9bb3657a2eeab
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-21 12:09:23 +0100

    Modification du dictionnaire des mois
    Changement du type du dictionnaire `mois` de `int` à `string`.
    Mise à jour des valeurs associées aux mois : "Juin" devient "sixième", "Septembre" devient "neuvième", et "Mars" est corrigé en "troisième".
    Suppression de l'entrée pour "Juin" et ajustement de l'affichage pour refléter ces modifications.

--- P1/P1C5.4/Program.cs
+++ P1/P1C5.4/Program.cs
@@ -1,19 +1,20 @@
-﻿IDictionary<string, int> mois = new Dictionary<string, int>();
+﻿IDictionary<string, string> mois = new Dictionary<string, string>();
 
 // TODO : Remplacer les noms par des constantes de type chaine de caractères
-mois.Add("Juin", 6);
-mois.Add("Septembre", 9);
-mois.Add("Mars", 5);
+mois.Add("Juin", "sixième");
+mois.Add("Septembre", "neuvième");
+mois.Add("Mars", "cinquième");
 
 // TODO : Corriger la valeur de Mars avec (3)
+mois["Mars"] = "troisième";
 
 
 // TODO : Retirer Juin
-
+mois.Remove("Juin");
 
 // Afficher le contenu du dictionnaire
 Console.WriteLine("La liste des mois est :");
-foreach (KeyValuePair<string, int> clePaire in mois)
+foreach (KeyValuePair<string, string> clePaire in mois)
 {
-    Console.WriteLine(clePaire.Key.ToString() + " est le numéro du mois de " + clePaire.Value + " de l'année ");
+    Console.WriteLine(clePaire.Key.ToString() + " est le "+ clePaire.Value + " mois de l'année ");
 }
