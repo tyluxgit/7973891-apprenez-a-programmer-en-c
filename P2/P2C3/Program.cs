cc8ea8238ea97515826a2a6111f91fae728b915a
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-21 12:36:44 +0100

    Ajout d'une boucle for pour afficher un message
    Implémentation d'une boucle `for` qui affiche la phrase « Je m'amuse comme un fou ! » cinq fois. La ligne de commentaire TODO a été conservée.

--- P2/P2C3/Program.cs
+++ P2/P2C3/Program.cs
@@ -1,1 +1,6 @@
 ﻿//TODO : Créer une boucle for et while qui affiche à 5 reprises la phrase, « Je m'amuse comme un fou ! ».
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Je m'amuse comme un fou !");
+}
