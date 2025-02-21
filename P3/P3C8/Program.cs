5a2354ae133ac3e9bfa3396149544bf90c1f14a9
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-21 15:26:39 +0100

    Ajout de la classe Program et d'un menu utilisateur
    Le fichier `Program.cs` a été mis à jour pour inclure le namespace `P3C8.Models` et définir la classe `Program` avec la méthode `Main`. Des commentaires ont été ajoutés pour guider l'utilisateur sur l'affichage d'un menu et la gestion des options saisies, y compris les erreurs potentielles liées aux options invalides.

--- P3/P3C8/Program.cs
+++ P3/P3C8/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using P3C8.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // TODO: L'utilisateur doit appuyer sur Entrée pour afficher le menu
+
+        /* TODO: Afficher un menu avec un message et les options suivantes: 
+         Veuillez sélectionner une option ci-dessous :
+        [I] Voir les informations sur le titulaire du compte
+        [CS] Compte courant - Consulter le solde
+        [CD] Compte courant - Déposer des fonds
+        [CR] Compte courant - Retirer des fonds
+        [ES] Compte épargne - Consulter le solde
+        [ED] Compte épargne - Déposer des fonds
+        [ER] Compte épargne - Retirer des fonds
+        [X] Quitter*/
+
+        //TODO: L'utilisateur doit saisir une option du menu (majuscule ou minuscule) et appuyer sur Entrée, le programme doit afficher un message d'erreur si l'option n'est pas valide
+
+    }
+}
