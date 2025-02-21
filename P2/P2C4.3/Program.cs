a257d14b80dd37418f8543dadd1e8a941b9dda06
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-21 13:08:09 +0100

    Gestion des exceptions pour le traitement des températures
    Le code a été modifié pour encapsuler la logique de traitement des températures dans un bloc `try`, permettant de gérer les exceptions potentielles. La liste `temperaturesEnregistreDegresCelcius` est maintenant initialisée à l'intérieur du bloc `try`, et les températures sont ajoutées à partir des arguments de la ligne de commande. En cas d'erreur, un message d'erreur est affiché. La logique de calcul et d'affichage de la température moyenne a également été déplacée à l'intérieur du bloc `try`.

--- P2/P2C4.3/Program.cs
+++ P2/P2C4.3/Program.cs
@@ -1,14 +1,21 @@
 ﻿using P2C4._3;
 
-List<int> temperaturesEnregistreDegresCelcius = new List<int>();
-
-// remplir la liste à partir des valeurs fournies comme arguments en ligne de commande
-foreach (string stringRepresentationTemperature in args)
+try
 {
-    int temperature = int.Parse(stringRepresentationTemperature);
-    temperaturesEnregistreDegresCelcius.Add(temperature);
-}
+    List<int> temperaturesEnregistreDegresCelcius = new List<int>();
 
-// Calculer et afficher la température moyenne
-int moyenneTemperature = MathSimple.CalculMoyenne(temperaturesEnregistreDegresCelcius);
-Console.WriteLine("La température moyenne est " + moyenneTemperature);
+    // remplir la liste à partir des valeurs fournies comme arguments en ligne de commande
+    foreach (string stringRepresentationTemperature in args)
+    {
+        int temperature = int.Parse(stringRepresentationTemperature);
+        temperaturesEnregistreDegresCelcius.Add(temperature);
+    }
+
+    // Calculer et afficher la température moyenne
+    int moyenneTemperature = MathSimple.CalculMoyenne(temperaturesEnregistreDegresCelcius);
+    Console.WriteLine("La température moyenne est " + moyenneTemperature);
+}
+catch (Exception ex) when (ex is FormatException || ex is IndexOutOfRangeException || ex is DivideByZeroException)
+{
+    Console.WriteLine("Erreur : Merci d'indiquer des entiers en paramètre");
+}
