b016d82c7289e77ea10b7b95827edab62caeeb10
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-23 17:59:17 +0100

    Ajout de la gestion des transactions dans les comptes
    Modifications apportées au fichier `Account.cs` :
    - Ajout de l'espace de noms `Account_Definition`.
    - Introduction d'une liste de transactions pour chaque compte.
    - Mise à jour des messages d'affichage pour le format monétaire.
    - Ajout des méthodes `RecordTransaction` et `DisplayTransactionHistory`.
    
    Modifications dans `Program.cs` :
    - Clarification des messages d'invite.
    - Réorganisation de la logique de la boucle principale.
    - Appel de `DisplayTransactionHistory` lors de la sortie du programme.
    
    Modifications dans `Tools.cs` :
    - Changement de l'espace de noms en `Tools_Definition`.
    - Rendre la classe `Tools` publique.
    - Ajout de la classe `Transaction` pour stocker les détails des transactions.

--- P3/P3C8/Program.cs
+++ P3/P3C8/Program.cs
@@ -1,74 +1,72 @@
-﻿using P3C8;
+﻿using Account_Definition;
+using System.Text;
+using Tools_Definition;
 
-class Program
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Tools.WaitingUser();
+string choix = "";
+// Création d'un compte par défaut
+Account defaultAccountHolder = new Account("John Doe", 123456, 500, 2000);
+
+while (choix != "X")
 {
-    static void Main()
+    Console.WriteLine("Veuillez sélectionner une option ci - dessous :");
+    Console.WriteLine("[I] Voir les informations sur le titulaire du compte");
+    Console.WriteLine("[CS] Compte courant - Consulter le solde");
+    Console.WriteLine("[CD] Compte courant - Déposer des fonds");
+    Console.WriteLine("[CR] Compte courant - Retirer des fonds");
+    Console.WriteLine("[ES] Compte épargne - Consulter le solde");
+    Console.WriteLine("[ED] Compte épargne - Déposer des fonds");
+    Console.WriteLine("[ER] Compte épargne - Retirer des fonds");
+    Console.WriteLine("[X] Quitter");
+
+    choix = Console.ReadLine().ToUpper();
+    switch (choix)
     {
-        Tools.WaitingUser();
-        string choix = "";
-        // Création d'un compte par défaut
-        Account defaultAccountHolder = new Account("John Doe", 123456, 500, 2000);
-
-        while (choix != "X")
-        {
-            Console.WriteLine("Veuillez sélectionner une option ci - dessous :");
-            Console.WriteLine("[I] Voir les informations sur le titulaire du compte");
-            Console.WriteLine("[CS] Compte courant - Consulter le solde");
-            Console.WriteLine("[CD] Compte courant - Déposer des fonds");
-            Console.WriteLine("[CR] Compte courant - Retirer des fonds");
-            Console.WriteLine("[ES] Compte épargne - Consulter le solde");
-            Console.WriteLine("[ED] Compte épargne - Déposer des fonds");
-            Console.WriteLine("[ER] Compte épargne - Retirer des fonds");
-            Console.WriteLine("[X] Quitter");
-
-            choix = Console.ReadLine().ToUpper();
-            switch (choix)
-            {
-                case "I":
-                    Console.WriteLine("Voir les informations sur le titulaire du compte");
-                    defaultAccountHolder.AccountHolderInfo(); Tools.WaitingUser();
-                    break;
-                case "CS":
-                    Console.WriteLine("Compte courant - Consulter le solde");
-                    defaultAccountHolder.CheckingAccountBalance(); Tools.WaitingUser();
-                    break;
-                case "CD":
-                    Console.WriteLine("Compte courant - Déposer des fonds");
-                    Console.WriteLine("Veuillez saisir le montant à déposer :");
-                    double amountCD = Convert.ToDouble(Console.ReadLine());
-                    defaultAccountHolder.DepositCheckingAccount(amountCD); Tools.WaitingUser();
-                    break;
-                case "CR":
-                    Console.WriteLine("Compte courant - Retirer des fonds");
-                    Console.WriteLine($"Veuillez saisir le montant à retirer (Max:{defaultAccountHolder.CheckingBalance:C2})");
-                    double amountCR = Convert.ToDouble(Console.ReadLine());
-                    defaultAccountHolder.WithdrawCheckingAccount(amountCR); Tools.WaitingUser();
-                    break;
-                case "ES":
-                    Console.WriteLine("Compte épargne - Consulter le solde");
-                    defaultAccountHolder.SavingAccountBalance(); Tools.WaitingUser();
-                    break;
-                case "ED":
-                    Console.WriteLine("Compte épargne - Déposer des fonds");
-                    Console.WriteLine("Veuillez saisir le montant à déposer :");
-                    double amountED = Convert.ToDouble(Console.ReadLine());
-                    defaultAccountHolder.DepositSavingAccount(amountED); Tools.WaitingUser();
-                    break;
-                case "ER":
-                    Console.WriteLine("Compte épargne - Retirer des fonds");
-                    Console.WriteLine($"Veuillez saisir le montant à retirer (Max:{defaultAccountHolder.SavingBalance:C2})");
-                    double amountER = Convert.ToDouble(Console.ReadLine());
-                    defaultAccountHolder.WithdrawSavingAccount(amountER);Tools.WaitingUser();
-                    break;
-                case "X":
-                    Console.WriteLine("Quitter");
-                    break;
-                default:
-                    Console.WriteLine("Option invalide");
-                    break;
-            }
-        }
-
-     
+        case "I":
+            Console.WriteLine("Voir les informations sur le titulaire du compte");
+            defaultAccountHolder.AccountHolderInfo(); Tools.WaitingUser();
+            break;
+        case "CS":
+            Console.WriteLine("Compte courant - Consulter le solde");
+            defaultAccountHolder.CheckingAccountBalance(); Tools.WaitingUser();
+            break;
+        case "CD":
+            Console.WriteLine("Compte courant - Déposer des fonds");
+            Console.WriteLine("Quel montant souhaitez-vous déposer ?");
+            double amountCD = Convert.ToDouble(Console.ReadLine());
+            defaultAccountHolder.DepositCheckingAccount(amountCD); Tools.WaitingUser();
+            break;
+        case "CR":
+            Console.WriteLine("Compte courant - Retirer des fonds");
+            Console.WriteLine($"Quel montant souhaitez-vous retirer ? (Max:{defaultAccountHolder.CheckingBalance:C2})");
+            double amountCR = Convert.ToDouble(Console.ReadLine());
+            defaultAccountHolder.WithdrawCheckingAccount(amountCR); Tools.WaitingUser();
+            break;
+        case "ES":
+            Console.WriteLine("Compte épargne - Consulter le solde");
+            defaultAccountHolder.SavingAccountBalance(); Tools.WaitingUser();
+            break;
+        case "ED":
+            Console.WriteLine("Compte épargne - Déposer des fonds");
+            Console.WriteLine("Quel montant souhaitez-vous déposer ?");
+            double amountED = Convert.ToDouble(Console.ReadLine());
+            defaultAccountHolder.DepositSavingAccount(amountED); Tools.WaitingUser();
+            break;
+        case "ER":
+            Console.WriteLine("Compte épargne - Retirer des fonds");
+            Console.WriteLine($"Quel montant souhaitez-vous retirer ? (Max:{defaultAccountHolder.SavingBalance:C2})");
+            double amountER = Convert.ToDouble(Console.ReadLine());
+            defaultAccountHolder.WithdrawSavingAccount(amountER); Tools.WaitingUser();
+            break;
+        case "X":
+            Console.WriteLine("Quitter");
+            defaultAccountHolder.DisplayTransactionHistory();
+            break;
+        default:
+            Console.WriteLine("Option invalide");
+            break;
     }
 }
--- P3/P3C8/Tools.cs
+++ P3/P3C8/Tools.cs
@@ -1,11 +1,28 @@
-﻿namespace P3C8;
+﻿namespace Tools_Definition;
 
-    class Tools
+public class Tools
+{
+    public static void WaitingUser()
     {
-        public static void WaitingUser()
+        Console.WriteLine("Appuyez sur Entrée pour afficher le menu.");
+        Console.ReadLine();
+    }
+}
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string AccountType { get; set; }
+        public string TransactionType { get; set; }
+        public double Amount { get; set; }
+
+        public Transaction(string accountType, string transactionType, double amount)
         {
-            Console.WriteLine("Appuyez sur Entrée pour continuer...");
-            Console.ReadLine();
+            Date = DateTime.Now;
+            AccountType = accountType;
+            TransactionType = transactionType;
+            Amount = amount;
         }
     }
+
+
 
