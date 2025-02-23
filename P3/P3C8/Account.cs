fd8bae05a4ddb106f716b716fdb548ea7814d273
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-02-23 17:08:17 +0100

    Remplacer AccountHolder par Account avec nouvelles méthodes
    La classe `AccountHolder` a été supprimée et remplacée par la classe `Account`, qui gère désormais les soldes des comptes courant et épargne. Des méthodes pour afficher les informations du titulaire, consulter les soldes, et effectuer des dépôts et retraits ont été ajoutées. Le fichier `Program.cs` a été mis à jour pour créer un compte par défaut et utiliser les nouvelles méthodes de la classe `Account`.

--- P3/P3C8/Account.cs
+++ P3/P3C8/Account.cs
@@ -1,19 +1,62 @@
 ﻿namespace P3C8;
 
-    public class AccountHolder(string name, int accountid)
+    public class Account(string name, int accountid, double checkingbalance, double savingbalance)
     {
         public string Name { get; set; } = name;
         public int AccountId { get; set; } = accountid;
+        public double CheckingBalance { get; set; } = checkingbalance;
+        public double SavingBalance { get; set; } = savingbalance;
 
-        public static void AccountHolderInfo()
+
+    public void AccountHolderInfo() 
         {
-            AccountHolder accountHolder = new AccountHolder("John Doe", 123456);
-            Console.WriteLine($"Nom : {accountHolder.Name} - Numéro de compte : {accountHolder.AccountId}");
+            Console.WriteLine($"Nom : {Name} - Numéro de compte : {AccountId}");
         }
-    }
-    public class Account
+
+    public void CheckingAccountBalance()
     {
-        public Account()
+        Console.WriteLine($"Solde du compte courant : {CheckingBalance}");
+    }
+
+    public void SavingAccountBalance()
+    {
+        Console.WriteLine($"Solde du compte épargne : {SavingBalance}");
+    }
+
+    public void DepositCheckingAccount(double amount)
+    {
+        CheckingBalance += amount;
+        Console.WriteLine($"Dépôt de {amount} sur le compte courant effectué avec succès");
+    }
+
+    public void DepositSavingAccount(double amount)
+    {
+        SavingBalance += amount;
+        Console.WriteLine($"Dépôt de {amount} sur le compte épargne effectué avec succès");
+    }
+
+    public void WithdrawCheckingAccount(double amount)
+    {
+        if (CheckingBalance - amount < 0)
         {
+            Console.WriteLine("Fonds insuffisants");
+        }
+        else
+        {
+            CheckingBalance -= amount;
+            Console.WriteLine($"Retrait de {amount} sur le compte courant effectué avec succès");
         }
     }
+    public void WithdrawSavingAccount(double amount)
+    {
+        if (CheckingBalance - amount < 0)
+        {
+            Console.WriteLine("Fonds insuffisants");
+        }
+        else
+        {
+            CheckingBalance -= amount;
+            Console.WriteLine($"Retrait de {amount} sur le compte courant effectué avec succès");
+        }
+    }
+}
--- P3/P3C8/Program.cs
+++ P3/P3C8/Program.cs
@@ -6,6 +6,8 @@ class Program
     {
         Tools.WaitingUser();
         string choix = "";
+        // Création d'un compte par défaut
+        Account defaultAccountHolder = new Account("John Doe", 123456, 500, 2000);
 
         while (choix != "X")
         {
@@ -24,25 +26,39 @@ class Program
             {
                 case "I":
                     Console.WriteLine("Voir les informations sur le titulaire du compte");
-                    AccountHolder.AccountHolderInfo(); Tools.WaitingUser();
+                    defaultAccountHolder.AccountHolderInfo(); Tools.WaitingUser();
                     break;
                 case "CS":
                     Console.WriteLine("Compte courant - Consulter le solde");
+                    defaultAccountHolder.CheckingAccountBalance(); Tools.WaitingUser();
                     break;
                 case "CD":
                     Console.WriteLine("Compte courant - Déposer des fonds");
+                    Console.WriteLine("Veuillez saisir le montant à déposer :");
+                    double amountCD = Convert.ToDouble(Console.ReadLine());
+                    defaultAccountHolder.DepositCheckingAccount(amountCD); Tools.WaitingUser();
                     break;
                 case "CR":
                     Console.WriteLine("Compte courant - Retirer des fonds");
+                    Console.WriteLine("Veuillez saisir le montant à retirer :");
+                    double amountCR = Convert.ToDouble(Console.ReadLine());
+                    defaultAccountHolder.WithdrawCheckingAccount(amountCR); Tools.WaitingUser();
                     break;
                 case "ES":
                     Console.WriteLine("Compte épargne - Consulter le solde");
+                    defaultAccountHolder.SavingAccountBalance(); Tools.WaitingUser();
                     break;
                 case "ED":
                     Console.WriteLine("Compte épargne - Déposer des fonds");
+                    Console.WriteLine("Veuillez saisir le montant à déposer :");
+                    double amountED = Convert.ToDouble(Console.ReadLine());
+                    defaultAccountHolder.DepositSavingAccount(amountED); Tools.WaitingUser();
                     break;
                 case "ER":
                     Console.WriteLine("Compte épargne - Retirer des fonds");
+                    Console.WriteLine("Veuillez saisir le montant à retirer :");
+                    double amountER = Convert.ToDouble(Console.ReadLine());
+                    defaultAccountHolder.WithdrawSavingAccount(amountER); Tools.WaitingUser();
                     break;
                 case "X":
                     Console.WriteLine("Quitter");
